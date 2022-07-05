5bff31c3c2a74f03b9f78a04875717a947a8ade1
QuyDX <wolflavamc@gmail.com>
2022-07-05 22:59:30 +0700

    fix responsive

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,49 +9,49 @@ window.addEventListener('load', () => {
     }, 600);
 });
 // -----------------------------
-window.onload=function(){
+// Toggle Navbar
+const navToggler = document.querySelector('.nav--toggler');
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle('hide--scrolling');
+})
 
-    // Toggle Navbar
-    const navToggler = document.querySelector('.nav--toggler');
-    navToggler.addEventListener("click", () => {
-        hideSection();
-        toggleNavbar();
-        document.body.classList.toggle('hide--scrolling');
-    })
+function hideSection() {
+    document.querySelector('section.active').classList.toggle('fade--out')
+}
 
-    function hideSection() {
-        document.querySelector('section.active').classList.toggle('fade--out')
-    }
+function toggleNavbar() {
+    document.querySelector('.header').classList.toggle('active')
+}
+// -----------------------------
 
-    function toggleNavbar() {
-        document.querySelector('.header').classList.toggle('active')
-    }
-    // -----------------------------
-
-    // Active Section
-    document.addEventListener("click", (e) => {
-        if (e.target.classList.contains('link-item') && e.target.hash !== "") {
-            document.querySelector('.overlay').classList.add('active');
-            navToggler.classList.add('hide');
-            if (e.target.classList.contains('nav__item')) {
-                toggleNavbar();
-            } else {
-                hideSection();
-                document.body.classList.add('hide--scrolling');
-            }
-            setTimeout(() => {
-                document.querySelector('section.active').classList.remove('active', 'fade--out');
-                document.querySelector(e.target.hash).classList.add('active');
-                window.scrollTo(0,0);
-                document.body.classList.remove('hide--scrolling');
-                navToggler.classList.remove('hide');
-                document.querySelector('.overlay').classList.remove('active');
-            }, 500)
+// Active Section
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains('link-item') && e.target.hash !== "") {
+        document.querySelector('.overlay').classList.add('active');
+        navToggler.classList.add('hide');
+        if (e.target.classList.contains('nav__item')) {
+            toggleNavbar();
+        } else {
+            hideSection();
+            document.body.classList.add('hide--scrolling');
         }
-    })
-    // -----------------------------
+        setTimeout(() => {
+            document.querySelector('section.active').classList.remove('active', 'fade--out');
+            document.querySelector(e.target.hash).classList.add('active');
+            window.scrollTo(0,0);
+            document.body.classList.remove('hide--scrolling');
+            navToggler.classList.remove('hide');
+            document.querySelector('.overlay').classList.remove('active');
+        }, 500)
+    }
+})
+// -----------------------------
 
-    // About Tabs
+// About Tabs
+AboutTabs();
+function AboutTabs() {
     const tabsContainer = document.querySelector('.about__tabs');
     const aboutSection = document.querySelector('.about');
     
@@ -64,9 +64,12 @@ window.onload=function(){
             aboutSection.querySelector(target).classList.add('active');
         }
     })
-    // -----------------------------
+}
+// -----------------------------
 
-    // Portfolio Item Details Popup
+// Portfolio Item Details Popup
+PortfolioItemDetailsPopup();
+function PortfolioItemDetailsPopup() {
     document.addEventListener("click", (e) => {
         if (e.target.classList.contains('myworks__item--btn') || e.target.classList.contains('myworks__item--thumbnail')) {
             toggleMyWorksPopup();
@@ -74,29 +77,29 @@ window.onload=function(){
             myWorksItemDetails(e.target.parentElement);
         }
     })
-
+    
     function toggleMyWorksPopup() {
         document.querySelector('.myworks__popup').classList.toggle('open');
         document.body.classList.toggle('hide--scrolling');
         document.querySelector('.main').classList.toggle('fade--out')
     }
-
+    
     document.querySelector('.mwp__header--close').addEventListener("click", toggleMyWorksPopup);
     document.addEventListener("click", (e) => {
         if(e.target.classList.contains('mwp__inner')) {
             toggleMyWorksPopup();
         }
     })
-
+    
     function myWorksItemDetails(myWorksItem) {
         document.querySelector('.mwp__header--thumbnail img').src = 
         myWorksItem.querySelector('.myworks__item--thumbnail img').src;
-
+    
         document.querySelector('.mwp__content--header h3').innerHTML = 
         myWorksItem.querySelector('.myworks__item--title').innerHTML;
-
+    
         document.querySelector('.mwp__content--body').innerHTML = 
         myWorksItem.querySelector('.myworks__item--details').innerHTML;
     }
-    // -----------------------------
 }
+// -----------------------------
